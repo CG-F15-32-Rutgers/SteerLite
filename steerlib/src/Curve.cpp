96e9d84fee6ac91821ef702f54bc843160f66d3b
ah007 <rockonhoxha@gmail.com>
2015-10-04 10:53:15 -0400

    Update Curve.cpp

--- steerlib/src/Curve.cpp
+++ steerlib/src/Curve.cpp
@@ -177,21 +177,36 @@ Point Curve::useHermiteCurve(const unsigned int nextPoint, const float time)
 Point Curve::useCatmullCurve(const unsigned int nextPoint, const float time)
 {
 	Point newPosition;
+	float normalTime, intervalTime;
 
-	//================DELETE THIS PART AND THEN START CODING===================
-	static bool flag = false;
-	if (!flag)
-	{
-		std::cerr << "ERROR>>>>Member function useCatmullCurve is not implemented!" << std::endl;
-		flag = true;
-	}
-	//=========================================================================
-
-
-	// Calculate time interval, and normal time required for later curve calculations
-
-	// Calculate position at t = time on Catmull-Rom curve
+	CurvePoint p1 = controlPoints[nextPoint - 1];
+	CurvePoint p2 = controlPoints[nextPoint];
 	
-	// Return result
+	Vector tan1;
+	Vector tan2;
+	
+	normalTime = time - p1.time;
+	intervalTime = p2.time - p1.time;
+	
+	float s = normalTime/intervalTime;
+	
+	if (nextPoint == 1) {
+		tan1 = 2*((p2-p1)) - ((controlPoints[nextPoint+1]-p1)/2);
+		tan2 = (controlPoints[nextPoint+1]-p1)/2;
+	} else if (nextPoint == (controlPoints.size() - 1)) {
+		tan1 = (p2-controlPoints[nextPoint-2])/2;
+		tan2 = tan1 - (2*(p1-p2));
+	} else {
+		tan1 = (p2-controlPoints[nextPoint-2])/2;
+		tan2 = (controlPoints[nextPoint+1]-p1)/2;
+	}
+	
+	float h1 = 2*pow(s, 3) - 3*pow(s,2) + 1;
+	float h2 = -2*pow(s,3) + 3*pow(s,2);
+	float h3 = pow(s,3) - 2*pow(s,2) + s;
+	float h4 = pow(s,3) - pow(s,2);
+	
+	newPosition = h1*p1 + h2*p2 + h3*t1 + h4*t2;
+	
 	return newPosition;
 }
